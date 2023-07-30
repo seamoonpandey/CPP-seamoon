6e6eb64f6b18ac63f32f91aa6c021d1a262c566e
Seamoon Pandey <hackedasocial@gmail.com>
2023-07-30 22:37:44 +0545

    saved object data

--- file_handling/disk_obj.cpp
+++ file_handling/disk_obj.cpp
@@ -1,27 +1,65 @@
-#include<iostream>
-#include<fstream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
-class Person{
-
+class Person {
 protected:
-	string name;
-	short age;
+    string name;
+    short age;
+
 public:
-	void getData(){
-		cout<<"Enter name:\t";
-		getline(cin,name);
-		cout<<"Enter your age:\t";
-		cin>>age;
-		getchar();
-	}
+    void getData() {
+        cout << "Enter name:\t";
+        getline(cin, name);
+        cout << "Enter your age:\t";
+        cin >> age;
+        cin.ignore(); // To consume the newline character left by cin
+    }
+
+    void showData() {
+        cout << "Name:\t" << name << endl;
+        cout << "Age:\t" << age << endl;
+    }
+
+    // Save person data to a file
+    void saveToFile(const string& filename) {
+        ofstream outfile(filename);
+        if (outfile) {
+            outfile << name << endl;
+            outfile << age << endl;
+            cout << "Person data saved to file: " << filename << endl;
+        } else {
+            cout << "Error opening file: " << filename << endl;
+        }
+    }
+
+    // Load person data from a file
+    void loadFromFile(const string& filename) {
+        ifstream infile(filename);
+        if (infile) {
+            getline(infile, name);
+            infile >> age;
+            cout << "Person data loaded from file: " << filename << endl;
+        } else {
+            cout << "Error opening file: " << filename << endl;
+        }
+    }
 };
 
-int main(){
-	Person person;
-	person.getData();
+int main() {
+    Person person;
 
-	ofstream outfile("text_files/Person.txt",ios::binary);
-	outfile.write(reinterpret_cast<char*>(&person), sizeof(person));
-	return 0;
+    // Get data from the user
+    person.getData();
+
+    // Save person data to a file
+    person.saveToFile("text_files/Person.txt");
+
+    // Load person data from a file
+    person.loadFromFile("text_files/Person.txt");
+
+    // Display the data
+    person.showData();
+
+    return 0;
 }
