b9697a84c03b3d8ad8a3b78e060894386905de3f
Seamoon Pandey <hackedasocial@gmail.com>
2023-07-30 21:38:06 +0545

    add basic input output from file

---
+++ file_handling/fileone.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+string str1 = "I love Me.";
+string str2 = "I can die for me.";
+string str3 = "I am seamoon.";
+
+ofstream outfile("love.txt");
+
+outfile<<str1<<endl<<str2<<endl<<str3<<endl;
+
+cout<< "Written in file"<<endl;
+
+return 0;
+
+}
---
+++ file_handling/filetwo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+
+string str1,str3,str2 ;
+
+ifstream infile("love.txt");
+
+getline(infile,str1);
+getline(infile,str2);
+getline(infile,str3);
+
+cout<<str1<<str2<<str3<<endl;
+
+// cout<< "Written in file"<<endl;
+
+return 0;
+
+}
